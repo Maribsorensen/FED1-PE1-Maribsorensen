b283e3a494f78290e1d299e80043cf927e7caeee
Maribsorensen <148907991+Maribsorensen@users.noreply.github.com>
2024-09-26 14:55:50 +0200

    testing file paths

--- scripts/shared/constants.mjs
+++ scripts/shared/constants.mjs
@@ -4,4 +4,4 @@ export const REGISTER_ENDPOINT = `${BASE_API_URL}auth/register`;
 export const LOGIN_ENDPOINT = `${BASE_API_URL}auth/login`;
 export const BLOG_ENDPOINT = `${BASE_API_URL}blog/posts/MariBS`;
 
-export const ABSOLUTE_URL_PATH = `https://maribsorensen.github.io/FED1-PE1-Maribsorensen/`
+// export const ABSOLUTE_URL_PATH = `https://maribsorensen.github.io/FED1-PE1-Maribsorensen/`
--- scripts/shared/initializeNav.mjs
+++ scripts/shared/initializeNav.mjs
@@ -1,4 +1,4 @@
-import { ABSOLUTE_URL_PATH } from "./constants.mjs";
+// import { ABSOLUTE_URL_PATH } from "./constants.mjs";
 
 
 function createNavItem(text, href = "#", id = "") {
@@ -15,7 +15,7 @@ function createHamburgerMenu() {
   const unorderedList = document.createElement("ul");
   unorderedList.className = "hamburger-menu";
 
-  const manager = createNavItem("Manage posts", "/post/manage.html");
+  const manager = createNavItem("Manage posts", "../post/manage.html");
   const logout = createNavItem("Logout", "#", "logout");
 
   unorderedList.append(manager, logout);
@@ -44,8 +44,8 @@ function createOwnerMenu(username) {
 
 function createUserMenu() {
   const unorderedList = document.createElement("ul");
-  unorderedList.appendChild(createNavItem("Login", `${ABSOLUTE_URL_PATH}account/login.html`));
-  unorderedList.appendChild(createNavItem("Register", `${ABSOLUTE_URL_PATH}account/register.html`));
+  unorderedList.appendChild(createNavItem("Login", `../account/login.html`));
+  unorderedList.appendChild(createNavItem("Register", `../account/register.html`));
   return unorderedList;
 };
 
